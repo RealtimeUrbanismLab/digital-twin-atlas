74f8d4c631b4026e18dc384ee7e8c59778046460
Realtime Urbanism Lab <fl385@cornell.edu>
2024-08-10 01:17:05 -0400

    Deploying to Github Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,10 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
+  pathPrefix: "/digital-twin-atlas", // Add this line
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://realtimeurbanismlab.github.io/digital-twin-atlas`, // Update this line
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -32,12 +24,9 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
   ],
-}
+};
