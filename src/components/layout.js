30ffa1a5514ec00e91c65dd847c7cfac694cf24d
Realtime Urbanism Lab <fl385@cornell.edu>
2024-08-15 15:58:58 -0400

    Layout error resolve

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-//import Header from "./header"
-//import "./layout.css"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,7 +23,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+
       <div
         style={{
           margin: `0 auto`,
